743b04ba3e85b42a8024cbc514fe99b3d0769bb8
Dmitrij <komissarenko312@gmail.com>
2022-08-27 23:30:09 +0300

    add sort to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @sort = {
+      :new => 'От новых к старым',
+      :old => 'От старых к новым',
+    }
+    field_sort = products_sort(params[:sort])
+    @products = Product.all.order(field_sort)
   end
 
   def show
@@ -12,6 +17,18 @@ class ProductsController < ApplicationController
   end
 
   def create
+  end
 
+  private
+
+  def products_sort(sort)
+    case sort
+    when 'old' #вначале старые
+      field_sort = {created_at: :asc}
+    when 'new' #вначале новые
+      field_sort = {created_at: :desc}
+    else 
+      field_sort = {created_at: :desc}
+    end
   end
 end
